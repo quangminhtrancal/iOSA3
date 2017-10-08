866267340326ad9e68b112a4a9e383e171cb7a5d
minhtranzentech <leap.ahead.tran@gmail.com>
2017-10-07 20:01:49 -0600

    First project with the add-in view of meal type

---
+++ FoodTracker/FoodTracker/MealtypeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealtypeTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Minh on 2017-10-07.
+//  Copyright © 2017 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class MealtypeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
